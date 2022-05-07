21ea49ee86927b25f2f91dd89265a0051dc786f2
knks <skaneko449@gmail.com>
2022-05-07 22:39:53 +0900

    Question CRUD

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,19 +1,39 @@
 class Admin::QuestionsController < ApplicationController
-  def index
-  end
+  before_action :get_question, only: %i[show edit update destroy]
+
+  def index ;end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to admin_questions_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render new
+    end
   end
 
-  def edit
-  end
+  def show ;end
+
+  def edit ;end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
+  def get_question
+    @question = Question.find(params[:id])
   end
 end
