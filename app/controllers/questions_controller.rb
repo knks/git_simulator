b4d969c243cee92c095a59098bf459fb203124cb
knks <skaneko449@gmail.com>
2022-05-15 01:37:10 +0900

    choice mnodel

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,16 @@
 class QuestionsController < ApplicationController
+  before_action :get_question, only: %i[show]
+  skip_before_action :require_login, only: %i[index show]
+
   def index
+    @questions = Question.all
+  end
+
+  def show ;end
+
+  private
+
+  def get_question
+    @question = Question.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
-  get 'questions', to: 'questions#index'
   
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
   resources :users, only: %i[new create]
+  resources :questions, only: %i[index show]
   
   namespace :admin do
     resources :questions
